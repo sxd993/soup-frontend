3d83876bf33438a6230e3a0f7c7d65913e02b477
Vladimir <103881264+sxd993@users.noreply.github.com>
2025-12-25 18:35:38 +0500

    подфикс под макет

--- src/widgets/News/ScrollNewsList/ui/ScrollNewsList.tsx
+++ src/widgets/News/ScrollNewsList/ui/ScrollNewsList.tsx
@@ -5,7 +5,7 @@ import { NEWS, NewsListItem } from "@/entities"
 export const ScrollNewsList = () => {
     return (
         <div className="w-full grid grid-cols-1 gap-6 items-stretch min-h-[350px]">
-            <div className="grid md:grid-cols-3 lg:grid-cols-4 content-between gap-5">
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 content-between gap-5">
                 {NEWS.map((n) => (
                     <NewsListItem
                         key={n.id}
