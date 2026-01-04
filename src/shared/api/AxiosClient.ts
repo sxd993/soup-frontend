4b3d0f7985aa1ac783dfd33b7c78a06b6c1e4509
sxd993 <gamhotik2005@mail.ru>
2026-01-04 23:02:30 +0500

    порты поменял для бека

--- src/shared/api/AxiosClient.ts
+++ src/shared/api/AxiosClient.ts
@@ -3,7 +3,7 @@ import { queryClient } from '@/app/providers/react-query/client'
 import { attachAuthInterceptors } from './interceptors/attachAuth'
 
 export const AxiosClient = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://localhost:3005/',
   withCredentials: true,
 });
 
